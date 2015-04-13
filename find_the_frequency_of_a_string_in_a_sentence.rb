071a4371016913dbac68249ca9f692748a29212d
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-13 22:57:25 +0700

    Add example finding the frequency of a string in a sentence

---
+++ find_the_frequency_of_a_string_in_a_sentence.rb
@@ -0,0 +1,11 @@
+def find_frequency(sentence, word)
+  sentence = sentence.downcase
+  arrSentence = sentence.split(' ') # or arrSentence = sentence.split
+  return arrSentence.count(word)
+end
+
+puts find_frequency('Ruby is The best language in the World', 'the')
+
+def find_frequency(sentence, word)
+  sentence.downcase.split.count(word.downcase)
+end
