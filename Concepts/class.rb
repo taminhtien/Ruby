469d38ecd19f2efff5c7f0741972d6570116f931
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-15 15:54:17 +0700

    Add class syntaxes

---
+++ Concepts/class.rb
@@ -0,0 +1,15 @@
+# Grouping Objects
+puts 1.is_a?(Integer)
+puts 1.is_a?(String)
+
+# Class are people too
+puts 1.class.class
+
+# Create an instance of an object
+Object.new
+
+# Make your class
+class Rectangle
+  def perimeter
+  end
+end
