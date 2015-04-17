08b42e0a26f21866e40ab1420bcb2f0d07ff0690
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-17 14:13:40 +0700

    Add exercise finding non-duplicate values in an array

---
+++ Exercises/find_non-duplicate_values_in_an_array.rb
@@ -0,0 +1,18 @@
+def non_duplicated_values(values)
+  values2 = []
+  values.each do |i|
+    if values.count(i) == 1
+      values2 << i
+    end
+  end
+  values2
+end
+
+=begin
+def non_duplicated_values(values)
+  values.find_all { |x| values.count(x) == 1 }
+end
+=end
+
+values = [1,2,2,3,3,4,5]
+puts non_duplicated_values(values)
